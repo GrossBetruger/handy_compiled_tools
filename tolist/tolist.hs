7250978b9ea7e4a4d31884796e9e99e79aa2f051
Oren Koriat <oren@cynerio.co>
2018-11-21 17:08:02 +0200

    single quotes option added!

--- tolist/tolist.hs
+++ tolist/tolist.hs
@@ -7,13 +7,13 @@ clean s = filter (/="") s
 
 pystring s = pylistWrap $ pystringWrap $ intercalate  "','" s
 pystringWrap s = "'" ++ s ++ "'"
-pylistWrap s = "(" ++ s ++ ")"
+pylistWrap s = "[" ++ s ++ "]"
 
 checkSingleQuotesArg args
-  | length args == 2 = False
+  | length args < 2 = False
   | otherwise = head (tail args) == "single"
 
 main = do
      args <- getArgs
      let raw = head args
-     print $ clean (splitDelim raw) 
+     if checkSingleQuotesArg args == True then putStrLn $ pystring $ clean (splitDelim raw) else print $ clean (splitDelim raw) 
