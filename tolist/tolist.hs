46894e37d827d82169c3ddfbe3ce6157af4cbd14
Oren Koriat <oren@cynerio.co>
2018-11-21 17:02:26 +0200

    added pystring style output cli option and logic, still unused

--- tolist/tolist.hs
+++ tolist/tolist.hs
@@ -1,10 +1,19 @@
 import Data.List.Split
 import System.Environment
+import Data.List
 
 splitDelim s = splitWhen (\c -> (c==' ' || c==',' || c=='\n' || c=='\t')) s
 clean s = filter (/="") s
 
+pystring s = pylistWrap $ pystringWrap $ intercalate  "','" s
+pystringWrap s = "'" ++ s ++ "'"
+pylistWrap s = "(" ++ s ++ ")"
+
+checkSingleQuotesArg args
+  | length args == 2 = False
+  | otherwise = head (tail args) == "single"
+
 main = do
      args <- getArgs
      let raw = head args
-     print $ clean (splitDelim raw)
+     print $ clean (splitDelim raw) 
