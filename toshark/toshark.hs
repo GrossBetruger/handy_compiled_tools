41e446ebcbcd1b29368f19cf4512547fc383f94c
Oren Koriat <oren@cynerio.co>
2019-11-13 18:50:40 +0200

    bug fix - second arg now override default string "host"

--- toshark/toshark.hs
+++ toshark/toshark.hs
@@ -6,22 +6,19 @@ import Data.List
 splitDelim s = splitWhen (\c -> (c==' ' || c==',' || c=='\n' || c=='\t')) s
 clean s = filter (/="") s
 
-checkLogicArg args
-  | length args < 2 = False
-  | otherwise = head (tail args) == "and"
+getFieldName args
+  | length args > 2 = "host"
+  | otherwise = head (tail args)
 
 addOrLogic terms = intercalate " or " terms
 
 addAndLogic terms = intercalate " and " terms
 
-toHosts terms = map ("host " ++) terms
+toShark field terms = map ((field ++ " ") ++) terms
 
 brackets expresion = "(" ++ expresion ++ ")"
 
 main = do
      args <- getArgs
      let raw = head args
-     if checkLogicArg args == True
-        then putStrLn $ brackets $ addAndLogic $ toHosts $ clean (splitDelim raw)
-     else putStrLn $ brackets $ addOrLogic $ toHosts $ clean (splitDelim raw)
-
+     putStrLn $ brackets $ addOrLogic $ toShark (getFieldName args) (clean (splitDelim raw))
